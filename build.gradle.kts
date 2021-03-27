97c8988c60b4e4a66cde608de44231022c699e29
Tobias Wiese <38278057+twiese99@users.noreply.github.com>
2021-03-27 19:46:11 +0100

    Entry for GitHub packages temporarily removed.
    GitHub currently does not allow public / unauthorized querying of packages. Therefore I use JitPack.io

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,16 +11,6 @@ repositories {
 }
 
 publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/twiese99/AxiosKt")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-            }
-        }
-    }
     publications.withType<MavenPublication> {
         pom {
             name.set("AxiosKt")
